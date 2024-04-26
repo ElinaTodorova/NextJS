a14c00203b91b13cbf9e15b7be61f917d958c0c1
ElinaTodorova <elina.todorova93@gmail.com>
2024-04-26 15:33:32 +0200

    Bold text in paragraphs

--- src/components/Typography/SectionParagraph/SectionParagraph.jsx
+++ src/components/Typography/SectionParagraph/SectionParagraph.jsx
@@ -1,4 +1,37 @@
-import "./SectionParagraph.sass"
-export default function SectionParagraph({data}) {
-    return <p className="paragraph">{data}</p>
+import "./SectionParagraph.sass";
+import parse from 'html-react-parser';
+
+export default function SectionParagraph({ data }) {
+    let formattedData = [];
+    let startIndex = data.indexOf("<&");
+    
+    while (startIndex !== -1) {
+        const beforeBold = data.substring(0, startIndex);
+        if (beforeBold !== "") { 
+            formattedData.push(beforeBold);
+        }
+        
+        const endIndex = data.indexOf("&>", startIndex);
+        
+        if (endIndex === -1) {
+            break;
+        }
+        
+        const boldWords = data.substring(startIndex + 2, endIndex);
+        
+        formattedData.push(<b key={startIndex}>{boldWords}</b>);
+        
+        startIndex = endIndex + 2;
+        
+        data = data.substring(startIndex);
+        
+        startIndex = data.indexOf("<&");
+    }
+    
+    if (data !== "") { 
+        formattedData.push(data);
+    }
+    
+    return <p className="paragraph">{formattedData}</p>;
 }
+
